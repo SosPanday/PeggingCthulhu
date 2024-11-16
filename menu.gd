bcbdeb8b0b37286c39a3a4653bc4a4fcb8ffa86f
Eren <eren.tepe@hof-university.de>
2024-11-16 17:41:56 +0100

    💩

--- menu.gd
+++ menu.gd
@@ -2,21 +2,38 @@ extends Control
 
 var master_bus = AudioServer.get_bus_index("Master")
 
-func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://game_proto.tscn")
-	
-	
+const RUN_SCENE = preload("res://scenes/run/run.tscn")
+
+@export var run_startup: RunStartup
+
+@onready var continue_button: Button = %Continue
 
 
-func _on_how_to_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://how_to_play.tscn")
-	
-	
+func _ready() -> void:
+	get_tree().paused = false
+	continue_button.disabled = SaveGame.load_data() == null
+
+
+func _on_continue_pressed() -> void:
+	if run_startup == null:
+		run_startup = RunStartup.new()
+		
+	Global.run_startup = run_startup
+	run_startup.type = RunStartup.Type.CONTINUED_RUN
+	get_tree().change_scene_to_packed(RUN_SCENE)
+
+
+func _on_new_run_pressed() -> void:
+	if run_startup == null:
+		run_startup = RunStartup.new()
+		
+	print("Start new Run with")
+	run_startup.type = RunStartup.Type.NEW_RUN
+	get_tree().change_scene_to_packed(RUN_SCENE)
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-
 
 func _on_h_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(master_bus, value)
