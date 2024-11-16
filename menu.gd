6f55125fcaa7ee6534afb3aa624f166e30131394
Eren <eren.tepe@hof-university.de>
2024-11-16 02:36:19 +0100

    💩

--- menu.gd
+++ menu.gd
@@ -1,7 +1,6 @@
 extends Control
 
-
-
+var master_bus = AudioServer.get_bus_index("Master")
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://game_proto.tscn")
@@ -17,3 +16,12 @@ func _on_how_to_play_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(master_bus, value)
+	
+	if value == -30:
+		AudioServer.set_bus_mute(master_bus, true)
+	else:
+		AudioServer.set_bus_mute(master_bus, false)
